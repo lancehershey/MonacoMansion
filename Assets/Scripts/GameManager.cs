6a00d651ead6dc272bf07398bbb6efc4c585c046
lancehershey <lancehershey@gmail.com>
2015-05-28 12:34:11 -0600

    Initial Unity Project Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public int numberOfAis = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
