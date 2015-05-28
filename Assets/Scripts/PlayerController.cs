f2e369371e00039b0fe8395bcebbfb51ec369d36
lancehershey <lancehershey@gmail.com>
2015-05-28 13:11:03 -0600

    Added different asset types to test git

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10;
+
+	private Rigidbody2D rb2D;
+
+	void Awake () {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+		{
+			transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+		if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime, transform.position.z);
+		}
+		if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
+		}
+
+	}
+}
